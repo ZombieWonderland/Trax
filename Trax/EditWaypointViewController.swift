567af09eaa7e65ef9888a1bc21b5195f84bc704b
Dan Livingston <danthewireman@gmail.com>
2016-03-15 06:42:10 -0700

    Adding new images to waypoint

--- Trax/EditWaypointViewController.swift
+++ Trax/EditWaypointViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class EditWaypointViewController: UIViewController, UITextFieldDelegate
+class EditWaypointViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
 
     // this is the model
@@ -21,6 +22,7 @@ class EditWaypointViewController: UIViewController, UITextFieldDelegate
         // Update text fields (UI) with data from the waypoint (model)
         nameTextField?.text = waypointToEdit?.name
         infoTextField?.text = waypointToEdit?.info
+        updateImage()
     }
     
     
@@ -28,10 +30,73 @@ class EditWaypointViewController: UIViewController, UITextFieldDelegate
         presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    
+    // MARK: - View Controller Lifecycle
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         observeTextFields()
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.becomeFirstResponder()
+        updateUI()
+    }
+    
+    // get rid of text field observers
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let observer = nameTextFieldObserver {
+            NSNotificationCenter.defaultCenter().removeObserver(observer)
+        }
+        if let observer = infoTextFieldObserver {
+            NSNotificationCenter.defaultCenter().removeObserver(observer)
+        }
+        
+    }
+    
+    // MARK: - Images
+    
+    var imageView = UIImageView()
+    
+    
+    @IBOutlet weak var imageViewContainer: UIView! {
+        didSet {
+            imageViewContainer.addSubview(imageView)
+        }
+    }
+    
+    @IBAction func takePhoto()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .Camera
+            picker.mediaTypes = [kUTTypeImage as String]
+            picker.delegate = self
+            picker.allowsEditing = true
+            presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var image = info[UIImagePickerControllerEditedImage] as? UIImage
+        if image == nil {
+            image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        }
+        
+        imageView.image = image
+        makeRoomForImage()
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    // MARK: - Text fields
     
     // these are just cookies
     var nameTextFieldObserver: NSObjectProtocol?
@@ -52,29 +117,57 @@ class EditWaypointViewController: UIViewController, UITextFieldDelegate
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        nameTextField.becomeFirstResponder()
-        updateUI()
-    }
-    
-    // get rid of text field observers
-    override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(animated)
-        if let observer = nameTextFieldObserver {
-            NSNotificationCenter.defaultCenter().removeObserver(observer)
-        }
-        if let observer = infoTextFieldObserver {
-            NSNotificationCenter.defaultCenter().removeObserver(observer)
-        }
 
-    }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
 
-
-
 }
+
+extension EditWaypointViewController
+{
+    func updateImage() {
+        if let url = waypointToEdit?.imageURL {
+            let qos = QOS_CLASS_USER_INITIATED
+            dispatch_async(dispatch_get_global_queue(qos, 0)) { [weak self] in
+                if let imageData = NSData(contentsOfURL: url) {
+                    //if url == self?.waypointToEdit?.imageURL {
+                        if let image = UIImage(data: imageData) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self?.imageView.image = image
+                                self?.makeRoomForImage()
+                            }
+                        }
+                    //}
+                }
+            }
+        }
+    }
+    
+    func makeRoomForImage() {
+        var extraHeight: CGFloat = 0
+        if imageView.image?.aspectRatio > 0 {
+            if let width = imageView.superview?.frame.size.width {
+                let height = width / imageView.image!.aspectRatio
+                extraHeight = height - imageView.frame.height
+                imageView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+            }
+        } else {
+            extraHeight = -imageView.frame.height
+            imageView.frame = CGRectZero
+        }
+        preferredContentSize = CGSize(width: preferredContentSize.width, height: preferredContentSize.height + extraHeight)
+    }
+}
+
+extension UIImage {
+    var aspectRatio: CGFloat {
+        return size.height != 0 ? size.width / size.height: 0
+    }
+}
+
+
+
+
